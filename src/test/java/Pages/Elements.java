b92cb53104ec72fb0f2dd15d04fdcf7b6f7394ed
HP <gamze.batmazzz@gmail.com>
2025-05-07 20:53:18 +0100

    Test tamamlandi.

--- src/test/java/Pages/Elements.java
+++ src/test/java/Pages/Elements.java
@@ -1,12 +1,26 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Elements {
     public Elements() {
         PageFactory.initElements(GWD.getDriver(),this);
     }
+
+    @FindBy(xpath = "//*[@name='username']")
+    public WebElement username;
+
+    @FindBy(xpath = "//*[@name='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//*[@value='Log In']")
+    public WebElement loginbutton;
+
+    @FindBy(xpath = "//*[@id='rightPanel']/h1")
+    public WebElement loginErrorMessage;
 
 
 
